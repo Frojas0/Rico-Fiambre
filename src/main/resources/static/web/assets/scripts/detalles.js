46143a4d160493388e7335c9a6eae524094e3236
Facundo Rojas <facuroja@hotmail.com>
2023-06-02 20:52:06 -0300

    hoover productos solucionado

--- src/main/resources/static/web/assets/scripts/detalles.js
+++ src/main/resources/static/web/assets/scripts/detalles.js
@@ -1,69 +1,54 @@
 // const params =  new URLSearchParams(location.search)
 // let nombre = params.get("nombre")
-let nombre = "JAMON CRUDO"
+let nombre = "QUESO AZUL"
 
-const {createApp} = Vue
+const { createApp } = Vue
 const app = createApp({
-data(){
+    data() {
         return {
             productosPeso: [],
             productoUni: [],
-            nombreParams: undefined,
+            // nombreParams: undefined,
             productoFiltrado: undefined,
             tipoProducto: undefined,
             todosLosProductos: []
         }
     },
 
-    created(){
-        this.nombreParams = nombre
+    created() {
         this.getProductoPeso()
         this.getProductoUni()
     },
 
-    methods:{
-    getProductoPeso(){
+    methods: {
+        getProductoPeso() {
             axios.get('/api/productoPeso')
-            .then(elemento =>{
-                this.productosPeso = elemento.data
-                console.log(this.productosPeso)
-            })
+                .then(res => {
+                    this.todosLosProductos = this.todosLosProductos.concat(res.data)
+                    console.log(this.todosLosProductos);
+                    this.encontrarProducto();
+                })
+        },
+
+        getProductoUni() {
+            axios.get('/api/productoUni')
+                .then(res => {
+                    this.todosLosProductos = this.todosLosProductos.concat(res.data)
+                    console.log(this.todosLosProductos);
+                    this.encontrarProducto();
+                })
+        },
+
+        encontrarProducto() {
+            this.todosLosProductos.forEach(element => {
+                if (element.nombre === nombre) {
+                    this.productoFiltrado = element
+                }
+            });
+            // console.log(this.productoFiltrado)
+        },
+
     },
-
-    getProductoUni(){
-        axios.get('/api/productoUni')
-        .then(elemento =>{
-            this.productoUni = elemento.data
-            console.log(this.productoUni)
-            this.getFilterData()
-        })
-    },
-
-    getFilterData(){
-        for(let element of this.productosPeso){
-            this.todosLosProductos.push(element)
-        }
-
-        for(let element of this.productoUni){
-            this.todosLosProductos.push(element)
-
-        }
-        console.log(this.todosLosProductos)
-        
-        this.productoEncontrado()
-    },
-
-    productoEncontrado(){
-        this.todosLosProductos.forEach(element => {
-            if(element.nombre === nombre){
-                this.productoFiltrado = element
-                
-            }
-        });
-    console.log(this.productoFiltrado)
-    },
-
-},
 
 })
 app.mount('#vueApp')
