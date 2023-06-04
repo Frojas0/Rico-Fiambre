08c27b0d741043666b9eb987f785d96155e749f0
jennifermotaochoa <jmota@fi.uba.ar>
2023-06-04 17:44:23 -0300

    admin header y boton salir

--- src/main/resources/static/web/admin/admin.js
+++ src/main/resources/static/web/admin/admin.js
@@ -154,6 +154,28 @@ const app = createApp({
             }))
         },
 
+        cerrarSesion(){
+            Swal.fire({
+                title: '¿Estas seguro de salir?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: 'black',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: 'Cerrar sesion'
+              }).then(result => {
+                    if (result.isConfirmed){
+                        axios.post('/api/logout')
+                        .then(response =>  window.location.href="/web/index.html")
+                        .catch(error => Swal.fire({
+                            title: 'Error',
+                            text: error.response.data,
+                            icon: 'error'
+                        }))   
+                    }
+            })
+        },
+
         seleccionarFormCrear(){
             this.mostrarFormCrear = true;
             this.mostrarFormCrearDescuento = false;
