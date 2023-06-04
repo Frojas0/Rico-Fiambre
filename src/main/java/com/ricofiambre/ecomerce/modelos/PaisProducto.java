a3eb8661a02e189fc1688310c18703b8578e72f3
Facundo Rojas <facuroja@hotmail.com>
2023-06-04 02:12:06 -0300

    cambios varios

--- src/main/java/com/ricofiambre/ecomerce/modelos/PaisProducto.java
+++ src/main/java/com/ricofiambre/ecomerce/modelos/PaisProducto.java
@@ -1,10 +1,16 @@
 package com.ricofiambre.ecomerce.modelos;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum PaisProducto {
     NACIONAL,
     EUROPEO,
     ASIATICO,
     SUDAMERICANO,
-    NORTEAMERICANO,
+    NORTEAMERICANO;
 
+    public static List<PaisProducto> obtenerPaisProducto() {
+        return Arrays.asList(com.ricofiambre.ecomerce.modelos.PaisProducto.values());
+    }
 }
