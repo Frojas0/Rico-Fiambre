d36f4a8666e8774ba3044d23e999abaf3ef749b1
Facundo Rojas <facuroja@hotmail.com>
2023-06-05 21:56:08 -0300

    Agregado estilos en login / register

--- src/main/resources/static/web/assets/scripts/login.js
+++ src/main/resources/static/web/assets/scripts/login.js
@@ -6,24 +6,22 @@ const app = createApp({
             contrasena: "",
         }
     },
-
     methods: {
-        iniciarSesion(){
-            axios.post('/api/login','email=' + this.email + '&contrasena=' + this.contrasena)
-            .then(response => {
-                if(this.email == "admin@admin.com"){
-                    window.location.href = "/web/admin/admin.html"
-                }
-                else{
-                    window.location.href = "/web/index.html"
-                }
+        iniciarSesion() {
+            axios.post('/api/login', 'email=' + this.email + '&contrasena=' + this.contrasena)
+                .then(response => {
+                    if (this.email == "admin@admin.com") {
+                        window.location.href = "/web/admin/admin.html"
+                    }
+                    else {
+                        window.location.href = "/web/index.html"
+                    }
                 })
-            
-            .catch(error => Swal.fire({
-                title: 'Error',
-                text: error.response.data,
-                icon: 'error'
-            }))
+                .catch(error => Swal.fire({
+                    title: 'Error',
+                    text: 'Correo o contraseña incorrectos',
+                    icon: 'error'
+                }))
         }
     }
 })
